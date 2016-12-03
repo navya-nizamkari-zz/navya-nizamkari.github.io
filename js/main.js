6c6a91002f50a1b690efcb262df8418a3799872a
navya-nizamkari <navyasri.tech@gmail.com>
2016-12-03 11:36:18 +0300

    Update

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+    /* default settings */
+    $('.venobox').venobox(); 
+
+    
+     jQuery('.item-masonry').hover(
+		function(){
+			$(this).find(".cover-item-gallery").fadeIn();
+		},
+		function(){
+			$(this).find(".cover-item-gallery").fadeOut();
+		}
+    );
+
+    var sizer = '.sizer4';
+
+    var container = $('#gallery');
+
+    container.imagesLoaded(function(){
+    	container.masonry({
+    		itemSelector: '.item-masonry',
+    		columnWidth: sizer,
+    		percentPosition: true
+    	});
+    });
+    
+});
